29c94527e56464b37acdd32a3ac8297a800c6539
An007coder <1576100794@qq.com>
2016-03-10 10:39:33 +0800

    添加了注释以及加了历炼buff

--- src/app/scenes/Lilian.lua
+++ src/app/scenes/Lilian.lua
@@ -13,8 +13,11 @@ local Lilian=class("Lilian", function ()
 end)
 
 function Lilian:ctor()
+	--初始化数据
 	self:initData()
+	--初始化UI等
 	self:init()
+	--定时器判断历炼的炮台是否完成历炼
 	self:update()
 end
 
@@ -29,26 +32,27 @@ end
 
 function Lilian:update()
 	local function judgeLiLianOver()
+		--判断是否有炮台正在历炼
 		if cc.UserDefault:getInstance():getBoolForKey(ISLILIAN) then
 			local nowTime = os.time()
 			local oldTime = cc.UserDefault:getInstance():getIntegerForKey(LILIANOFTIME)
 			local grade = cc.UserDefault:getInstance():getIntegerForKey("WUQI"..cc.UserDefault:getInstance():getIntegerForKey(LILIANTAINUM).."GRADE")
-			local hour = (nowTime - oldTime)-- / 3600
-
-			if hour >= 20 then
+			local hour = (nowTime - oldTime) / 3600
+			--判断炮台是否历炼完成
+			if hour >= (2*grade) then
 				cc.UserDefault:getInstance():setBoolForKey(ISLILIAN, false)
 				cc.UserDefault:getInstance():setIntegerForKey("WUQI"..cc.UserDefault:getInstance():getIntegerForKey(LILIANTAINUM).."GRADE", grade+1)
 				cc.UserDefault:getInstance():setIntegerForKey(LILIANTAINUM, 0)
-				self.paoTai:runAction(cc.Sequence:create(cc.FadeOut:create(0.5), cc.RemoveSelf:create()))
-				-- self.paoTai:removeFromParent()
+				self.paoTai:runAction(cc.Sequence:create(cc.FadeOut:create(0.3), cc.RemoveSelf:create()))
 				scheduler.unscheduleGlobal(self.handle)
 			end
 		end
 	end
-	self.handle = scheduler.scheduleGlobal(judgeLiLianOver, 1)
+	self.handle = scheduler.scheduleGlobal(judgeLiLianOver, 0.5)
 end
 
 function Lilian:init()
+	--背景
 	local bg = display.newSprite("LiLianScene/bg.png")
 	local scaleX = display.width/bg:getContentSize().width
 	local scaleY = display.height/bg:getContentSize().height
@@ -56,6 +60,7 @@ function Lilian:init()
 	bg:setPosition(cc.p(display.cx,display.cy))
 	self:addChild(bg)
 
+	--返回
 	local backBtn = cc.ui.UIPushButton.new({normal="back.png"},{scale9=true})
                    :onButtonClicked(function(event)
                    	display.replaceScene(StartScene.new())
@@ -63,9 +68,14 @@ function Lilian:init()
                    :pos(display.right-50, display.top-50)
                    :addTo(self,1)
 
+    --历炼台
 	local liLianTai = display.newSprite("LiLianScene/liLianTai.png")
 	liLianTai:setPosition(cc.p(display.width*4.0/6, display.cy-100))
 	self:addChild(liLianTai)
+	--历炼Buff
+	local liLianBuff = cc.ParticleSystemQuad:create("LiLianScene/buff.plist")
+	liLianBuff:setPosition(cc.p(100, 100))
+	liLianBuff:addTo(liLianTai)
 
 	-- local liLian_layer = display.newColorLayer(cc.c4b(200,200,0,255))
 	-- liLian_layer:setContentSize(cc.size(display.width*0.444, display.height*3/2))
@@ -73,6 +83,7 @@ function Lilian:init()
 	-- liLian_layer:pos(0, display.height)
 	-- liLian_layer:setPosition(cc.p(0, display.height))
 
+	--创建ScrollView
 	local liLian_Scroll = cc.ScrollView:create(cc.size(display.width*0.444, display.height))
 	liLian_Scroll:setDirection(1)
 	liLian_Scroll:setContentSize(cc.size(display.width*0.444, display.height*5/3))
@@ -80,6 +91,7 @@ function Lilian:init()
 	--liLian_Scroll:setContentOffset(300)
 	self:addChild(liLian_Scroll)
 
+	--创建ScrollView的内容
 	for i=1,5 do
 		local liLian_item = display.newSprite("wuqi"..i..".png")
 		liLian_item:setPosition(cc.p(display.width/10, display.height*(2*i - 1)/6))
@@ -94,6 +106,7 @@ function Lilian:init()
 		:setPosition(cc.p(display.width/10 + 100, display.height*(2*i - 1)/6))
 		:addTo(liLian_Scroll)
 
+		--历炼按钮
 		local liLianBtn = cc.ui.UIPushButton.new({normal="LiLianScene/liLian.png"}, {scale9=true})
 							:onButtonClicked(function(event)
 								if cc.UserDefault:getInstance():getBoolForKey(ISLILIAN) == false then
@@ -104,6 +117,7 @@ function Lilian:init()
 								self.paoTai:runAction(cc.Spawn:create(
 													cc.MoveTo:create(1.0, cc.p(display.width*4.0/6, display.cy)),
 													cc.ScaleTo:create(1.0, 1.0, 1.0)))
+								--存储数据
 								cc.UserDefault:getInstance():setBoolForKey(ISLILIAN, true)
 								cc.UserDefault:getInstance():setIntegerForKey(LILIANTAINUM, i)
 								cc.UserDefault:getInstance():setIntegerForKey(LILIANOFTIME, os.time())
@@ -113,6 +127,7 @@ function Lilian:init()
 							:addTo(liLian_Scroll)
 	end
 
+	--有炮台历炼时重新进入该Scene时创建历炼炮台
 	if cc.UserDefault:getInstance():getIntegerForKey(LILIANTAINUM) ~= 0 then
 		self.paoTai = display.newSprite("wuqi"..cc.UserDefault:getInstance():getIntegerForKey(LILIANTAINUM)..".png")
 		self.paoTai:setPosition(cc.p(display.width*4.0/6, display.cy))
